a710e4a2deaa98020baa795ffca5ceac7b7de575
gtschmid <gtschmid@googlemail.com>
2016-01-08 16:45:53 +0000

    CIP-1 #Comment test release to demonstrate Eclipse to Github to Jenkins to Jira

--- Continuous_Integration_Test_Code_Java/src/Continuous_Integration_Test_Code_Java.java
+++ Continuous_Integration_Test_Code_Java/src/Continuous_Integration_Test_Code_Java.java
@@ -2,10 +2,6 @@ public class Continuous_Integration_Test_Code_Java {
 	   public static void main(String[] args) {
 	        // Prints "Hello, World" to the terminal window.
 	        System.out.println("This is Continuous Integration Test Java Code");
-	        System.out.println("First code release using Git-Jira smart commits");
+	        System.out.println("Code release using Git-Jira smart commits. Add Jira issue Key plus comment");
         }
 }
-
-
-
-
