3a962c0c0e18ef40572697cc38529cedbc80b795
gtschmid <gtschmid@googlemail.com>
2016-01-08 12:08:47 +0000

    First commit

---
+++ Continuous_Integration_Test_Code_Java/src/Continuous_Integration_Test_Code_Java.java
@@ -0,0 +1,10 @@
+public class Continuous_Integration_Test_Code_Java {
+	   public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("This is Continuous Integration Test Java Code nnnnn");
+        }
+}
+
+
+
+
