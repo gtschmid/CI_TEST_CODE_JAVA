4395d41f3ad2f2537b8cb89e0939c9d3b9d51656
gtschmid <gtschmid@googlemail.com>
2016-01-08 16:38:15 +0000

    CIP-1 #First end to end CI test

--- Continuous_Integration_Test_Code_Java/src/Continuous_Integration_Test_Code_Java.java
+++ Continuous_Integration_Test_Code_Java/src/Continuous_Integration_Test_Code_Java.java
@@ -2,7 +2,7 @@ public class Continuous_Integration_Test_Code_Java {
 	   public static void main(String[] args) {
 	        // Prints "Hello, World" to the terminal window.
 	        System.out.println("This is Continuous Integration Test Java Code");
-	        System.out.println("Code release : alsdhlaskhdlkashdl with Jira reference");
+	        System.out.println("First code release using Git-Jira smart commits");
         }
 }
 
