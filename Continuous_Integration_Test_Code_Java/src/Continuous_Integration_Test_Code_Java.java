ca2375268601b55437965107e1bfea6f994d1561
gtschmid <gtschmid@googlemail.com>
2016-01-08 16:20:24 +0000

    CIP-1
    commit 4

--- Continuous_Integration_Test_Code_Java/src/Continuous_Integration_Test_Code_Java.java
+++ Continuous_Integration_Test_Code_Java/src/Continuous_Integration_Test_Code_Java.java
@@ -2,7 +2,7 @@ public class Continuous_Integration_Test_Code_Java {
 	   public static void main(String[] args) {
 	        // Prints "Hello, World" to the terminal window.
 	        System.out.println("This is Continuous Integration Test Java Code");
-	        System.out.println("Code release : three");
+	        System.out.println("Code release : four with Jira reference");
         }
 }
 
